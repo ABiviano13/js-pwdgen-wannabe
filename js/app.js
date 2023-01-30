7ba5592550db29c23efd22bcffc1f6a032b5fd06
ABiviano13 <abiviano0@outlook.com>
2023-01-30 15:36:42 +0100

    FEAT variabile const,  prompt + concatenazione per la password

--- js/app.js
+++ js/app.js
@@ -1,5 +1,21 @@
-// 1. Chiedi all'utente il suo nome
-// 2. Chiedi all'utente il suo cognome
-// 3. Chiedi all'utente il suo colore preferito
-// 4. Chiedi all'utente un numero 
-// 5. Genera password
+// 0. Definisci la variabile della password
+const titleElement = document.getElementById('password');
+// 1. Definisci le variabili delle informazioni richieste
+let nome;
+let cognome;
+let colore;
+let numero;
+// 2. Chiedi all'utente il suo nome
+nome = prompt ("Digita il tuo nome?");
+console.log("la variabile nome è =", nome)
+// 3. Chiedi all'utente il suo cognome
+cognome = prompt ("Digita il tuo cognome");
+console.log("la variabile cognome è =", cognome)
+// 4. Chiedi all'utente il suo colore preferito
+colore = prompt ("Digita il tuo colore preferito");
+console.log("la variabile colore è =", colore)
+// 5. Chiedi all'utente un numero 
+numero = prompt ("Digita un numero");
+console.log("la variabile numero è =", numero)
+// 6. Genera password
+password.innerHTML = nome + cognome + colore + numero
