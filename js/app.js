b65ffdf7eb94210c028b73a1b623a470252c7c1b
ABiviano13 <abiviano0@outlook.com>
2023-01-30 15:17:03 +0100

    START

---
+++ js/app.js
@@ -0,0 +1,5 @@
+// 1. Chiedi all'utente il suo nome
+// 2. Chiedi all'utente il suo cognome
+// 3. Chiedi all'utente il suo colore preferito
+// 4. Chiedi all'utente un numero 
+// 5. Genera password
